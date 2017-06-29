1cadae5e4ccba03a6a09a6c2f5232230f8cb9045
Stephan Pavlovic <stephan@railslove.com>
2017-06-29 17:43:30 +0200

    use a different way to fetch the Mime type data

--- spec/mime_pdf_spec.rb
+++ spec/mime_pdf_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe Mime::PDF do
+describe Mime[:pdf] do
   specify do
-    expect(Mime::PDF.to_sym).to eql :pdf
-    expect(Mime::PDF.to_s).to eql 'application/pdf'
+    expect(Mime[:pdf].to_sym).to eql :pdf
+    expect(Mime[:pdf].to_s).to eql 'application/pdf'
   end
 end
